bc81d8648defc3ae338aac5f87bb01044de77003
VIVID <thenextgeneration@naver.com>
2024-10-07 21:38:24 +0900

    이동 1차 구현

--- Assets/반의진/Scripts/르르Move.cs
+++ Assets/반의진/Scripts/르르Move.cs
@@ -7,8 +7,8 @@ public class 르르Move : MonoBehaviour
     private PlayerInput playerInput;
     private 르르InputAction playerInputActions;
 
-    public Vector2 inputVector;
-    float moveSpeed = 10f;
+
+    float moveSpeed = 2f;
     bool isFacingRight;
 
     // Start is called before the first frame update
@@ -16,69 +16,58 @@ public class 르르Move : MonoBehaviour
     {
         ruruBody = gameObject.GetComponent<Rigidbody2D>();
         playerInput = GetComponent<PlayerInput>();
-        르르InputAction playerInputActions = new 르르InputAction();
+        playerInputActions = new 르르InputAction();
         playerInputActions.Player.Enable();
         playerInputActions.Player.Jump.performed += Jump;
         playerInputActions.Player.Move.performed += Move;
         playerInputActions.Player.Move.performed += Flip;
     }
 
+
+
     private void FixedUpdate()
-    {
+    { 
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+        ruruBody.AddForce(new Vector2(inputVector.x, 0) * moveSpeed, ForceMode2D.Impulse);
+    }
+        void OnEnable()
+        {
+
+
+        }
+
+        void OnDisable()
+        {
+
+        }
+
+        public void Move(InputAction.CallbackContext context)
+        {
+            
+
+        }
+
+        public void Jump(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                Debug.Log("르르Jumping!");
+                ruruBody.AddForce(Vector3.up * 5f, ForceMode2D.Impulse);
+            }
+        }
+
+        public void Flip(InputAction.CallbackContext context)
+        {
+        /*
+            if (inputVector.x == 1)
+            {
+                isFacingRight = true;
+            }
+            else if (inputVector.x == -1)
+            {
+                isFacingRight = false;
+            }
+        */
+        }
         
     }
-
-    private void Update()
-    {
-      
-    }
-
-    void OnEnable()
-    {
-        
-
-    }
-
-    void OnDisable()
-    {
-        
-    }
-
-    public void Move(InputAction.CallbackContext context)
-    {
-        inputVector = context.ReadValue<Vector2>();
-
-        if (inputVector.x == 1 && isFacingRight)
-        {
-            ruruBody.velocity = new Vector2(inputVector.x * moveSpeed, ruruBody.velocity.y);
-            Debug.Log("르르Moving!");
-            Debug.Log(context);
-        }
-        else if(inputVector.x == -1 && !isFacingRight)
-        {
-            ruruBody.velocity = new Vector2(inputVector.x * -moveSpeed, ruruBody.velocity.y);
-        }
-
-    }
-
-    public void Jump(InputAction.CallbackContext context)
-    {
-        if(context.performed)
-        {
-            Debug.Log("르르Jumping!");
-            ruruBody.AddForce(Vector3.up * 5f, ForceMode2D.Impulse);
-        }
-    }
-
-    public void Flip(InputAction.CallbackContext context)
-    {
-        if(inputVector.x == 1)
-        {
-            isFacingRight = true;
-        }
-        else if(inputVector.x == -1)
-        {
-            isFacingRight= false;
-        }
-    }
-}
